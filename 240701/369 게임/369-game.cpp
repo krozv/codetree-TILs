16d2aaf31f07142bd6423141f0c0cc1e48b9dca1
krozv <krozv.code@gmail.com>
2024-07-01 06:33:41 +0900

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240701/369 게임/369-game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        if (i % 3 == 0){
+            cout << 0 << ' ';
+        } 
+        else if ((i%10) % 3 == 0){
+            cout << 0 << ' ';
+        }
+        else if (i >= 10 && (i-(i%10)) % 3 == 0){
+            cout << 0 << ' ';
+        }
+        else {
+            cout << i << ' ';
+        }
+    }
+    return 0;
+}
