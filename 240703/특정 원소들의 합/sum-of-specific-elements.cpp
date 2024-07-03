9427cd4eff0e175910fa220bac688a31b14a88ac
krozv <krozv.code@gmail.com>
2024-07-03 17:52:28 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4][4];
+    for (int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
+            cin >> arr[i][j];
+        }
+    }
+    int sum_val = 0;
+    for (int i=0; i<4; i++){
+        for (int j=0; j<=i; j++){
+            sum_val += arr[i][j];
+        }
+    }
+    cout << sum_val;
+    return 0;
+}
