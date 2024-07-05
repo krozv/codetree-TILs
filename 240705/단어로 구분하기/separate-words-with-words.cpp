26783fceb1d669cd331374692f37ff34e2a3bfec
krozv <krozv.code@gmail.com>
2024-07-05 16:29:14 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240705/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    for (int i=0; i<10; i++){
+        string str;
+        cin >> str;
+        cout << str << endl;
+    }
+    return 0;
+}
