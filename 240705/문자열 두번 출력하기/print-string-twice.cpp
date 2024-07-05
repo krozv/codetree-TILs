038bc32407fb613eaec8b46b2cb3b968bdb4ca22
krozv <krozv.code@gmail.com>
2024-07-05 09:12:08 +0900

    Title: 문자열 두번 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240705/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    for (int i=0; i<2; i++){
+        cin >> str;
+        cout << str << endl;
+    }
+    return 0;
+}
