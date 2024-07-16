bee535cb447fe3c8646ce9a5d377e3690b91b619
krozv <krozv.code@gmail.com>
2024-07-16 18:12:52 +0900

    Title: hashmap 기본, Time: 161ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/hashmap 기본/hashmap-basic.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    unordered_map<int, int> m;
+    for (int i=0; i<n; i++){
+        string command;
+        cin >> command;
+        // add
+        if (command == "add"){
+            int k, v;
+            cin >> k >> v;
+            m[k] = v;
+        }
+        // remove
+        else if (command == "remove"){
+            int k;
+            cin >> k;
+            m.erase(k);
+        }
+        // find
+        else if (command == "find"){
+            int k;
+            cin >> k;
+            if (m.find(k) != m.end()){
+                // hashmap에 존재할 경우
+                cout << m[k] << endl;
+            }
+            else {
+                cout << "None" << endl;
+            }
+        }
+    }
+    return 0;
+}
