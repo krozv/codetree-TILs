1479a6758112796eee698795786d308b888e009e
krozv <krozv.code@gmail.com>
2024-08-16 17:16:10 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printString(int N){
+    for (int i=0; i<N; i++){
+    cout << "12345^&*()_" << endl;
+    }
+}
+int main() {
+    int N;
+    cin >> N;
+    printString(N);
+    return 0;
+}
